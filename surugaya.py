7e1e8ffbf0875d1ee28bea2ab7191ac81ceaa01f
Ibe0366 <39725478+Ibe0366@users.noreply.github.com>
2018-06-04 13:08:58 +0900

    修正
    修正

--- surugaya.py
+++ surugaya.py
@@ -6,25 +6,33 @@ f = open('url.txt')
 lines2 = f.readlines() 
 f.close()
 
-from bs4 import BeautifulSoup
-import urllib.request as req
-import csv
-
-f = open('url.txt') 
-lines2 = f.readlines() 
-f.close()
-
+pt = []
+pl = []
+pa = []
 
 # 取得
 for line in lines2: 
 	url = "https://www.suruga-ya.jp/product/detail/" + line
 	res = req.urlopen(url)
 	soup = BeautifulSoup(res, 'html.parser')
-	title1 = soup.find('h2', id='item_title')
-	p_list = soup.find('p', id="price")
 
-# CSV 書き出し
-with open('price.csv', 'w') as _file:
-	writer = csv.writer(_file, lineterminator='\n' , "utf8")
-	writer.writerow([title1])
-	writer.writerow([p_list])
+	title1 = soup.select('#item_title')
+
+	for i in title1:
+	    pt = i.get_text().replace('\n', '').replace('\r', '')
+
+
+	p_list = soup.select('#price')
+
+	for i in p_list:
+	    pl = i.get_text().replace('\n', '').replace('\r', '')
+
+	pa += [pt, pl]
+	
+
+	# CSV 書き出し
+with open('price.csv', 'w', encoding='utf-8') as _file:
+	writer = csv.writer(_file, lineterminator='\n')
+	writer.writerow(pa)
+
+
