97130377607ac5070ca3b42a33806a0550d7249e
ASUKARU Shouhei <35398848+AsukaruShouhei@users.noreply.github.com>
2018-06-05 20:39:07 +0900

    do
    do

--- pyQt.py
+++ pyQt.py
@@ -1,11 +1,13 @@
 # /user/bin/pyrhon3
 # -*- coding: utf-8 -*-
-
+from bs4 import BeautifulSoup
+import urllib.request as req
+import csv
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QTableWidgetItem, QTableWidget)
-import logging
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QTableWidgetItem, QTableWidget, QMainWindow)
 
 data = []
+
 class tableTest01(QTableWidget):
     """   テーブルを形成する関数  """
     def __init__(self, data, *args):
@@ -16,10 +18,10 @@ class tableTest01(QTableWidget):
         self.resizeRowsToContents()
 
     def setdata(self):
-        #ヘッダーを作成
+        #  ヘッダーを作成
         horHeaders = ['対象サイトのURL', '商品名', '現在価格', '希望価格']
         self.setHorizontalHeaderLabels(horHeaders)
-        #テーブルにデータをセット
+        #  テーブルにデータをセット
         for n in range(len(self.data)):
             print("n=%d" % n)
             for m in range(len(self.data[n])):
@@ -29,6 +31,35 @@ class tableTest01(QTableWidget):
 
 
 
+def startScrapy():
+    try:
+        f = open('sample.csv')
+        lines2 = f.readlines()
+        f.close()
+        pa = []
+
+        # 取得
+        for line in lines2:
+            url = "https://www.suruga-ya.jp/product/detail/" + line
+            res = req.urlopen(url)
+            soup = BeautifulSoup(res, 'html.parser')
+            title1 = soup.select('#item_title')
+            if len(title1) > 0 :
+                for i in title1:
+                    pt = i.get_text().replace('\n', '')
+            else:
+                pt = 'Nothing Product Name'
+            p_list = soup.select('#price')
+            if len(p_list) > 0 :
+                for x in p_list:
+                    pl = x.get_text().replace('\n', '').replace('\r', '')
+            else:
+                pl = 'Nothing Product Price'
+            pa += [line, pt, pl, 0]
+            print(pa)
+        return pa
+    except:
+        print("失敗")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -41,10 +72,10 @@ if __name__ == '__main__':
     button = QPushButton('スクレイピングを開始', window) # button
     button.resize(255, 72)
     button.setStyleSheet("background-color:#F2FBEF; margin-top:25px; margin-left:25px; font-wight:bold; font-family:'Meiryo'; letter-spacing: 10px;")
+    button.clicked.connect(startScrapy)
 
-    table = tableTest01(data, 3, 4)
-    table.show()
+
+
     window.show()
 
     sys.exit(app.exec_())
-
